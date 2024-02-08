e2f9d53345a1e64f98eb51d9fb183ea3fc0a47dc
Philipe Loureiro <philipeloureiro@gmail.com>
2024-02-08 02:19:56 -0300

    Add App.js and index.js to dist/public/client

---
+++ dist/public/client/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = __importDefault(require("react"));
+const react_dom_1 = __importDefault(require("react-dom"));
+const App_1 = __importDefault(require("./App"));
+// Verifica se o ambiente é o navegador antes de executar o código que depende do document
+if (typeof document !== 'undefined') {
+    react_dom_1.default.render(react_1.default.createElement(react_1.default.StrictMode, null,
+        react_1.default.createElement(App_1.default, null)), document.getElementById('root'));
+}
